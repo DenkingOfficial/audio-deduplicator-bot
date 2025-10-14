95ab8b1979e4b50563c0a9e9fea5a4c2b2e16103
DenkingOfficial <shershnev.andrei@gmail.com>
2025-10-15 02:06:44 +0500

    Added audio message handler

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import F, Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -11,9 +11,13 @@ class SijufyDedupBot(Bot):
 
     def _register_handlers(self):
         self.dp.message(Command("start"))(self.command_start_handler)
+        self.dp.message(F.content_type.in_({"audio"}))(self.handle_audio_message)
 
     async def command_start_handler(self, message: Message) -> None:
         await message.answer("Hello! I'm a bot created with aiogram.")
+
+    async def handle_audio_message(self, message: Message) -> None:
+        await message.answer("Audio Handler is called")
 
     async def start(self):
         await self.dp.start_polling(self)
