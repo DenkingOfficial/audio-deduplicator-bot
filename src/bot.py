e1942418c5010420bc66bd1f93a117befe3fb526
DenkingOfficial <shershnev.andrei@gmail.com>
2025-10-15 03:28:17 +0500

    Added a lot of things

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from uuid import uuid4
 from aiogram import F, Bot, Dispatcher
 from aiogram.filters import Command
@@ -11,6 +12,16 @@ class SijufyDedupBot(Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dp = Dispatcher()
+
+        try:
+            self.allowed_channel_id = int(os.getenv("ALLOWED_CHANNEL_ID"))
+            logging.info(f"Bot will operate in channel ID: {self.allowed_channel_id}")
+        except (ValueError, TypeError):
+            logging.error(
+                "ALLOWED_CHANNEL_ID is not set or is not a valid integer. The bot will not work in any channel."
+            )
+            self.allowed_channel_id = None
+
         self._register_handlers()
         self.audio_processor = UniqueMusicStorageApp(
             similarity_threshold=float(os.getenv("SIMILARITY_THRESHOLD", "0.04"))
@@ -19,31 +30,68 @@ class SijufyDedupBot(Bot):
 
     def _register_handlers(self):
         self.dp.message(Command("start"))(self.command_start_handler)
-        self.dp.message(F.content_type.in_({"audio"}))(self.handle_audio_message)
+
+        self.dp.message(
+            Command("clear"),
+            F.from_user.id.in_(set(os.getenv("ADMIN_USER_IDS").split(","))),
+        )(self.command_clear_handler)
+
+        self.dp.message(F.content_type.in_({"audio"}))(
+            self.handle_private_audio_message
+        )
+
+        if self.allowed_channel_id:
+            self.dp.channel_post(
+                F.chat.id == self.allowed_channel_id, F.content_type.in_({"audio"})
+            )(self.handle_audio_channel_post)
 
     async def command_start_handler(self, message: Message) -> None:
-        await message.answer("Hello! I'm a bot created with aiogram.")
+        await message.answer(
+            "Hello! I am a SIJUFY audio deduplicator bot. Add me to your channel as an admin to get started."
+        )
 
-    async def handle_audio_message(self, message: Message) -> None:
-        file_ext = message.audio.file_name.split(".")[-1]
+    async def command_clear_handler(self, message: Message) -> None:
+        self.audio_processor.clear_db()
+        await message.answer("SIJUFY Audio Deduplicator database is cleared!")
+
+    async def handle_private_audio_message(self, message: Message) -> None:
+        await message.answer("This bot is designed to work in SIJUFY channel only.")
+
+    async def handle_audio_channel_post(self, channel_post: Message) -> None:
+        if not channel_post.audio or not channel_post.audio.file_name:
+            return
+
+        file_ext = channel_post.audio.file_name.split(".")[-1]
         if file_ext not in self.supported_extensions:
-            await message.answer("Audio is not supported.")
+            logging.warning(
+                f"Unsupported audio format received: {channel_post.audio.file_name}"
+            )
             return
 
         sample_dir = "temp"
         if not os.path.exists(sample_dir):
             os.makedirs(sample_dir)
+
         unique_id = uuid4()
         file_path = f"temp/{str(unique_id)}.{file_ext}"
-        await self.download(message.audio.file_id, file_path)
+
+        await self.download(channel_post.audio.file_id, file_path)
+
         processing_status = self.audio_processor.process_and_add_track(file_path)
+
         os.remove(file_path)
+
         if processing_status == ProcessingStatus.UNIQUE:
             return
         elif processing_status == ProcessingStatus.ERROR:
-            message.answer("An error occurred while processing the audio.")
+            logging.error(
+                f"An error occurred while processing audio: {channel_post.audio.file_name}"
+            )
         elif processing_status == ProcessingStatus.DUPLICATE:
-            await self.delete_message(message.chat.id, message.message_id)
+            await self.delete_message(channel_post.chat.id, channel_post.message_id)
+            logging.info(
+                f"Deleted duplicate audio message {channel_post.message_id} from channel {channel_post.chat.id}"
+            )
 
     async def start(self):
         await self.dp.start_polling(self)
